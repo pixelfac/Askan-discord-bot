55d213266a46ad7219f45fe00a274618efc1deba
Nathan Harris <43653484+pixelfac@users.noreply.github.com>
2021-01-30 15:03:39 -0500

    added formatting and cleaned up db cmd

--- commands/db.js
+++ commands/db.js
@@ -21,53 +21,6 @@ module.exports = {
 
 
 		message.channel.send(`I don't know what database entry has the name: \`${args[0]}\`.`)
-
-/*
-		switch (args[0]) {
-
-			case "mm":
-				console.log('printing allMartialMeleeWeapons')
-
-				reply += `List of all Martial Melee Weapons\nFormat: \`<name>\` : \`<code>\`\n\`\`\``;
-				buildWeaponReply(allMartialMeleeWeapons)
-				reply += "\`\`\`"
-				message.channel.send(reply);
-				break;
-
-			case "mr":
-				console.log('printing allMartialRangedWeapons')
-
-				reply += `List of all Martial Ranged Weapons\nFormat: \`<name>\` : \`<code>\`\n\`\`\``;
-				buildWeaponReply(allMartialRangedWeapons)
-				reply += "\`\`\`"
-				message.channel.send(reply);
-				break;
-
-			case "sm":
-				console.log('printing allSimpleMeleeWeapons')
-
-				reply += `List of all Simple Melee Weapons\nFormat: \`<name>\` : \`<code>\`\n\`\`\``;
-				buildWeaponReply(allSimpleMeleeWeapons)
-				reply += "\`\`\`"
-				message.channel.send(reply);
-				break;
-
-			case "sr":
-				console.log('printing allSimpleRangedWeapons')
-
-				reply += `List of all Simple Ranged Weapons\nFormat: \`<name>\` : \`<code>\`\n\`\`\``;
-				buildWeaponReply(allSimpleRangedWeapons)
-				reply += "\`\`\`"
-				message.channel.send(reply);
-				break;
-
-			default:
-				message.channel.send("This command takes a category and returns the codes of all the items in that category. These codes are used for inputting selections into this bot, like in the 'createChar' command.")
-				break;
-
-		}
-	*/
-
     },
 };
 
@@ -85,13 +38,6 @@ for (let filename of codeFiles) {
    listsOfItems[filename] = codeJSON;
 }
 
-/*
-const allMartialMeleeWeapons = require('../Dnd_equipment/martialMeleeWeapons_WithCodes.json')
-const allMartialRangedWeapons = require('../Dnd_equipment/martialRangedWeapons_WithCodes.json');
-const allSimpleMeleeWeapons = require('../Dnd_equipment/simpleMeleeWeapons_WithCodes.json');
-const allSimpleRangedWeapons = require('../Dnd_equipment/simpleRangedWeapons_WithCodes.json'); 
-*/
-
 //builds string of all items and codes formatted
 function buildList(object) {
 	reply = "```"
@@ -100,7 +46,9 @@ function buildList(object) {
 		for (let prop in object) {
 			if (prop == 'header')
 				continue;
-			reply += `${prop} : ${object[prop]}\n`
+			if (prop == 'ListName')
+				reply += `List of ${object[prop]}\n\n`
+			else reply += `${prop} : ${object[prop]}\n`
 		}
 	} catch (err) {
 		console.log("object does not have correct properties\n", err)
